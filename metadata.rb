cf9ab224023d085f5cf0563f02fd07132d01651e
Rainer Karcher <Rainer.Karcher@Cerner.com>
2014-09-05 10:35:37 -0500

    Added description to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name "fs-create"
 maintainer "Rainer Karcher"
 maintainer_email "rainer.tobias.karcher@gmail.com"
 license "All rights reserved"
-description ""
+description "This Chef cookbook leverages the existing lvm community cookbook in creating customized logical volumes and mounted filesystems."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version "0.1.0"
 
