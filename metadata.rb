a7fd93c2f39d75ba2020ac251655893ad8ff402b
Rainer Karcher <Rainer.Karcher@Cerner.com>
2014-09-05 10:34:46 -0500

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name "fs-create"
+maintainer "Rainer Karcher"
+maintainer_email "rainer.tobias.karcher@gmail.com"
+license "All rights reserved"
+description ""
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version "0.1.0"
+
+depends 'lvm'
