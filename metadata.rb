51bcb69b6208204e2d464f1402435df68de1becf
Rainer Karcher <Rainer.Karcher@Cerner.com>
2014-09-05 12:44:57 -0500

    Version bump (1.0.0)

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email "rainer.tobias.karcher@gmail.com"
 license "All rights reserved"
 description "This Chef cookbook leverages the existing lvm community cookbook in creating customized logical volumes and mounted filesystems."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.1.0"
+version "1.0.0"
 
 depends 'lvm'
